e1bb14f548a53cb1b030da74f0e357369f1caf76
Lucas Jacobson <ljacobson64@gmail.com>
2015-11-20 22:00:33 -0600

    compilers build in /userspace/l/lucasj

--- setup/build_compile.sh
+++ setup/build_compile.sh
@@ -4,13 +4,19 @@ function build_gmp() {
   cd $build_dir
   mkdir gmp
   cd gmp
-  wget https://gmplib.org/download/gmp/gmp-"$gmp_version".tar.bz2
+  gmp_tar=gmp-"$gmp_version".tar.bz2
+  wget --spider http://proxy.chtc.wisc.edu/SQUID/"$username"/"$gmp_tar"
+  if [ $? == 0 ]; then
+    wget http://proxy.chtc.wisc.edu/SQUID/"$username"/"$gmp_tar"
+  else
+    wget https://gmplib.org/download/gmp/"$gmp_tar"
+  fi
   tar -xjvf gmp-"$gmp_version".tar.bz2
   ln -s gmp-"$gmp_version" src
   mkdir bld
   cd bld
   ../src/configure --prefix="$compile_dir"/gmp
-  make -j $jobs  # 29168 kB
+  make -j $jobs  # 29168 kB mem
   make install
   export LD_LIBRARY_PATH="$compile_dir"/gmp/lib:"$LD_LIBRARY_PATH"
   cd $base_dir
@@ -20,14 +26,20 @@ function build_mpfr() {
   cd $build_dir
   mkdir mpfr
   cd mpfr
-  wget http://www.mpfr.org/mpfr-current/mpfr-"$mpfr_version".tar.gz
+  mpfr_tar=mpfr-"$mpfr_version".tar.gz
+  wget --spider http://proxy.chtc.wisc.edu/SQUID/"$username"/"$mpfr_tar"
+  if [ $? == 0 ]; then
+    wget http://proxy.chtc.wisc.edu/SQUID/"$username"/"$mpfr_tar"
+  else
+    wget http://www.mpfr.org/mpfr-current/"$mpfr_tar"
+  fi
   tar -xzvf mpfr-"$mpfr_version".tar.gz
   ln -s mpfr-"$mpfr_version" src
   mkdir bld
   cd bld
   ../src/configure --with-gmp="$compile_dir"/gmp \
                    --prefix="$compile_dir"/mpfr
-  make -j $jobs  # 23744 kB
+  make -j $jobs  # 23744 kB mem
   make install
   export LD_LIBRARY_PATH="$compile_dir"/mpfr/lib:"$LD_LIBRARY_PATH"
   cd $base_dir
@@ -37,7 +49,13 @@ function build_mpc() {
   cd $build_dir
   mkdir mpc
   cd mpc
-  wget ftp://ftp.gnu.org/gnu/mpc/mpc-"$mpc_version".tar.gz
+  mpc_tar=mpc-"$mpc_version".tar.gz
+  wget --spider http://proxy.chtc.wisc.edu/SQUID/"$username"/"$mpc_tar"
+  if [ $? == 0 ]; then
+    wget http://proxy.chtc.wisc.edu/SQUID/"$username"/"$mpc_tar"
+  else
+    wget ftp://ftp.gnu.org/gnu/mpc/"$mpc_tar"
+  fi
   tar -xzvf mpc-"$mpc_version".tar.gz
   ln -s mpc-"$mpc_version" src
   mkdir bld
@@ -45,7 +63,7 @@ function build_mpc() {
   ../src/configure --with-gmp="$compile_dir"/gmp \
                    --with-mpfr="$compile_dir"/mpfr \
                    --prefix="$compile_dir"/mpc
-  make -j $jobs  # 16340 kB
+  make -j $jobs  # 16340 kB mem
   make install
   export LD_LIBRARY_PATH="$compile_dir"/mpc/lib:"$LD_LIBRARY_PATH"
   cd $base_dir
@@ -55,7 +73,13 @@ function build_gcc() {
   cd $build_dir
   mkdir gcc
   cd gcc
-  wget http://www.netgull.com/gcc/releases/gcc-"$gcc_version"/gcc-"$gcc_version".tar.gz
+  gcc_tar=gcc-"$gcc_version".tar.gz
+  wget --spider http://proxy.chtc.wisc.edu/SQUID/"$username"/"$gcc_tar"
+  if [ $? == 0 ]; then
+    wget http://proxy.chtc.wisc.edu/SQUID/"$username"/"$gcc_tar"
+  else
+    wget http://www.netgull.com/gcc/releases/gcc-"$gcc_version"/"$gcc_tar"
+  fi
   tar -xzvf gcc-"$gcc_version".tar.gz
   ln -s gcc-"$gcc_version" src
   mkdir bld
@@ -65,39 +89,46 @@ function build_gcc() {
                    --with-mpfr="$compile_dir"/mpfr \
                    --with-mpc="$compile_dir"/mpc \
                    --prefix="$compile_dir"/gcc
-  make -j $jobs  # 766268 kB
+  make -j $jobs  # 766268 kB mem, 4973.13 on /tmp, 4982.38 on $HOME
   make install
   export LD_LIBRARY_PATH="$compile_dir"/gcc/lib:"$LD_LIBRARY_PATH"
   export LD_LIBRARY_PATH="$compile_dir"/gcc/lib64:"$LD_LIBRARY_PATH"
   cd $base_dir
 }
 
-function pack_compilers() {
+function pack_compile() {
   cd $base_dir
-  tar -pczvf $compilers_tar $compile_dir
+  tar -pczvf $compile_tar compile
+  mv $compile_tar $copy_dir
 }
 
 function cleanup() {
-  cd $base_dir
-  ls | grep -v $compilers_tar | xargs rm -rf
+  rm -rf $build_dir
+  rm -rf $compile_dir
+  cd $copy_dir
+  ls | grep -v $compile_tar | xargs rm -rf
 }
 
 # Compiler versions
-gmp_version=6.1.0
-mpfr_version=3.1.3
-mpc_version=1.0.3
-gcc_version=4.9.3
+export gmp_version=6.1.0
+export mpfr_version=3.1.3
+export mpc_version=1.0.3
+export gcc_version=4.9.3
 
 # Parallel jobs
-jobs=8
+export jobs=8
 
-# Tarball names
-compile_tar=compile.tar.gz
+# Username where tarballs are found (/squid/$username)
+export username=ljjacobson
+
+# Output tarball
+export compile_tar=compile.tar.gz
 
 # Directory names
-base_dir="$PWD"
-compile_dir="$base_dir"/compile
-build_dir="$base_dir"/build
+export copy_dir=$PWD
+export base_dir=$HOME
+export compile_dir="$base_dir"/compile
+export build_dir="$base_dir"/build
 mkdir -p $compile_dir
 mkdir -p $build_dir
 
@@ -107,5 +138,8 @@ build_mpfr
 build_mpc
 build_gcc
 
-pack_compilers
+# Make the output tarball
+pack_compile
+
+# Delete unneeded stuff
 cleanup
