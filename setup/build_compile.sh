f846f5e52b50d3a40d3fcd194e2464988d38dc35
Lucas Jacobson <ljacobson64@gmail.com>
2015-11-24 14:47:55 -0600

    code reuse

--- setup/build_compile.sh
+++ setup/build_compile.sh
@@ -1,131 +1,153 @@
 #!/bin/bash
 
+# Get tarball
+function get_tar() {
+  tarball=$1
+  shift
+
+  while test ${#} -gt 0; do
+    url=$1
+    if [ "$url" == "squid" ]; then
+      url=http://proxy.chtc.wisc.edu/SQUID/$username
+    fi
+    wget --spider $url/$tarball
+    if [ $? == 0 ]; then
+      wget $url/$tarball
+      return
+    fi
+    shift
+  done
+
+  echo $tarball not found
+}
+
+function build_package() {
+  name="$1"
+  tarball="$2"
+  tar_type="$3"
+  tar_url="$4"
+  folder="$5"
+  config_str="$6"
+  path_dirs="$7"
+  ldlpath_dirs="$8"
+
+  cd $build_dir
+  mkdir -p $name/bld
+  cd $name
+  get_tar $tarball squid $tar_url
+  tar -x"$tar_type"vf $tarball
+  ln -s $folder src
+  cd bld
+  config_str+=" "--prefix=$compile_dir/$name
+  ../src/configure $config_str
+  make -j $jobs
+  make install
+  cd $base_dir
+
+  for path_dir in $path_dirs; do
+    export PATH=$compile_dir/$name/$path_dir:$PATH
+  done
+  for ldlpath_dir in $ldlpath_dirs; do
+    export LD_LIBRARY_PATH=$compile_dir/$name/$ldlpath_dir:$LD_LIBRARY_PATH
+  done
+}
+
 # Build GMP
 function build_gmp() {
-  cd $build_dir
-  mkdir -p gmp/bld
-  cd gmp
-  gmp_tar=gmp-$gmp_version.tar.bz2
-  wget --spider http://proxy.chtc.wisc.edu/SQUID/$username/$gmp_tar
-  if [ $? == 0 ]; then
-    wget http://proxy.chtc.wisc.edu/SQUID/$username/$gmp_tar
-  else
-    wget https://gmplib.org/download/gmp/$gmp_tar
-  fi
-  tar -xjvf $gmp_tar
-  ln -s gmp-$gmp_version src
-  cd bld
-  config_string=
-  config_string+=" "--prefix=$compile_dir/gmp
-  ../src/configure $config_string
-  make -j $jobs  # j=12: 0:17.26 wall time, 29168 kB mem
-  make install
-  export LD_LIBRARY_PATH=$compile_dir/gmp/lib:$LD_LIBRARY_PATH
-  cd $base_dir
+  version=$1
+  name=gmp
+  tarball=$name-$version.tar.bz2
+  tar_type=j
+  tar_url=https://gmplib.org/download/gmp
+  folder=$name-$version
+
+  config_str=
+
+  path_dirs=
+  ldlpath_dirs="lib"
+
+  build_package "$name" "$tarball" "$tar_type" "$tar_url" "$folder" \
+                "$config_str" "$path_dirs" "$ldlpath_dirs"
 }
 
 # Build MPFR
 function build_mpfr() {
-  cd $build_dir
-  mkdir -p mpfr/bld
-  cd mpfr
-  mpfr_tar=mpfr-$mpfr_version.tar.gz
-  wget --spider http://proxy.chtc.wisc.edu/SQUID/$username/$mpfr_tar
-  if [ $? == 0 ]; then
-    wget http://proxy.chtc.wisc.edu/SQUID/$username/$mpfr_tar
-  else
-    wget http://www.mpfr.org/mpfr-current/$mpfr_tar
-  fi
-  tar -xzvf $mpfr_tar
-  ln -s mpfr-$mpfr_version src
-  cd bld
-  config_string=
-  config_string+=" "--with-gmp=$compile_dir/gmp
-  config_string+=" "--prefix=$compile_dir/mpfr
-  ../src/configure $config_string
-  make -j $jobs  # j=12: 0:07.28 wall time, 23744 kB mem
-  make install
-  export LD_LIBRARY_PATH=$compile_dir/mpfr/lib:$LD_LIBRARY_PATH
-  cd $base_dir
+  version=$1
+  name=mpfr
+  tarball=$name-$version.tar.gz
+  tar_type=z
+  tar_url=http://www.mpfr.org/mpfr-current
+  folder=$name-$version
+
+  config_str=
+  config_str+=" "--with-gmp=$compile_dir/gmp
+
+  path_dirs=
+  ldlpath_dirs="lib"
+
+  build_package "$name" "$tarball" "$tar_type" "$tar_url" "$folder" \
+                "$config_str" "$path_dirs" "$ldlpath_dirs"
 }
 
 # Build MPC
 function build_mpc() {
-  cd $build_dir
-  mkdir -p mpc/bld
-  cd mpc
-  mpc_tar=mpc-$mpc_version.tar.gz
-  wget --spider http://proxy.chtc.wisc.edu/SQUID/$username/$mpc_tar
-  if [ $? == 0 ]; then
-    wget http://proxy.chtc.wisc.edu/SQUID/$username/$mpc_tar
-  else
-    wget ftp://ftp.gnu.org/gnu/mpc/$mpc_tar
-  fi
-  tar -xzvf $mpc_tar
-  ln -s mpc-$mpc_version src
-  cd bld
-  config_string=
-  config_string+=" "--with-gmp=$compile_dir/gmp
-  config_string+=" "--with-mpfr=$compile_dir/mpfr
-  config_string+=" "--prefix=$compile_dir/mpc
-  ../src/configure $config_string
-  make -j $jobs  # j=12: 0:02.81 wall time, 16340 kB mem
-  make install
-  export LD_LIBRARY_PATH=$compile_dir/mpc/lib:$LD_LIBRARY_PATH
-  cd $base_dir
+  version=$1
+  name=mpc
+  tarball=$name-$version.tar.gz
+  tar_type=z
+  tar_url=ftp://ftp.gnu.org/gnu/mpc
+  folder=$name-$version
+
+  config_str=
+  config_str+=" "--with-gmp=$compile_dir/gmp
+  config_str+=" "--with-mpfr=$compile_dir/mpfr
+
+  path_dirs=
+  ldlpath_dirs="lib"
+
+  build_package "$name" "$tarball" "$tar_type" "$tar_url" "$folder" \
+                "$config_str" "$path_dirs" "$ldlpath_dirs"
 }
 
 # Build GCC
 function build_gcc() {
-  cd $build_dir
-  mkdir -p gcc/bld
-  cd gcc
-  gcc_tar=gcc-$gcc_version.tar.gz
-  wget --spider http://proxy.chtc.wisc.edu/SQUID/$username/$gcc_tar
-  if [ $? == 0 ]; then
-    wget http://proxy.chtc.wisc.edu/SQUID/$username/$gcc_tar
-  else
-    wget http://www.netgull.com/gcc/releases/gcc-$gcc_version/$gcc_tar
-  fi
-  tar -xzvf $gcc_tar
-  ln -s gcc-$gcc_version src
-  cd bld
-  config_string=
-  config_string+=" "--with-gmp=$compile_dir/gmp
-  config_string+=" "--with-mpfr=$compile_dir/mpfr
-  config_string+=" "--with-mpc=$compile_dir/mpc
-  config_string+=" "--prefix=$compile_dir/gcc
-  ../src/configure $config_string
-  make -j $jobs  # j=12: 18:35.61 wall time, 766024 kB mem
-  make install
-  export LD_LIBRARY_PATH=$compile_dir/gcc/lib:$LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=$compile_dir/gcc/lib64:$LD_LIBRARY_PATH
-  cd $base_dir
+  version=$1
+  name=gcc
+  tarball=$name-$version.tar.gz
+  tar_type=z
+  tar_url=http://www.netgull.com/gcc/releases/gcc-$gcc_version
+  folder=$name-$version
+
+  config_str=
+  config_str+=" "--with-gmp=$compile_dir/gmp
+  config_str+=" "--with-mpfr=$compile_dir/mpfr
+  config_str+=" "--with-mpc=$compile_dir/mpc
+  config_str+=" "--prefix=$compile_dir/gcc
+
+  path_dirs="bin"
+  ldlpath_dirs="lib lib64"
+
+  build_package "$name" "$tarball" "$tar_type" "$tar_url" "$folder" \
+                "$config_str" "$path_dirs" "$ldlpath_dirs"
 }
 
 # Build OpenMPI
 function build_openmpi() {
-  cd $build_dir
-  mkdir -p openmpi/bld
-  cd openmpi
-  openmpi_tar=openmpi-$openmpi_version.tar.gz
-  wget --spider http://proxy.chtc.wisc.edu/SQUID/$username/$openmpi_tar
-  if [ $? == 0 ]; then
-    wget http://proxy.chtc.wisc.edu/SQUID/$username/$openmpi_tar
-  else
-    wget http://www.open-mpi.org/software/ompi/v1.10/downloads/$openmpi_tar
-  fi
-  tar -xzvf $openmpi_tar
-  ln -s openmpi-$openmpi_version src
-  cd bld
-  config_string=
-  config_string+=" "--prefix=$compile_dir/openmpi
-  ../src/configure $config_string
-  make -j $jobs  # j=12: 3:26.41 wall time, 311132 kB mem
-  make install
-  export PATH=$compile_dir/openmpi/bin:$PATH
-  export LD_LIBRARY_PATH=$compile_dir/openmpi/lib:$LD_LIBRARY_PATH
-  cd $base_dir
+  version=$1
+  name=openmpi
+  tarball=$name-$version.tar.gz
+  tar_type=z
+  tar_url=http://www.open-mpi.org/software/ompi/v1.10/downloads
+  folder=$name-$version
+
+  config_str=
+  config_str+=" "--prefix=$compile_dir/openmpi
+
+  path_dirs="bin"
+  ldlpath_dirs="lib"
+
+  build_package "$name" "$tarball" "$tar_type" "$tar_url" "$folder" \
+                "$config_str" "$path_dirs" "$ldlpath_dirs"
 }
 
 # Pack compiler tarball
@@ -144,15 +166,8 @@ function cleanup() {
 }
 
 set -e
-export args="$@"
-export args=" "$args" "
-
-# Compiler versions
-export gmp_version=6.1.0
-export mpfr_version=3.1.3
-export mpc_version=1.0.3
-export gcc_version=4.9.3
-export openmpi_version=1.10.1
+args="$@"
+args=" "$args" "
 
 # Parallel jobs
 export jobs=12
@@ -172,12 +187,12 @@ mkdir -p $compile_dir
 mkdir -p $build_dir
 
 # Build compilers
-build_gmp
-build_mpfr
-build_mpc
-build_gcc
+build_gmp 6.1.0
+build_mpfr 3.1.3
+build_mpc 1.0.3
+build_gcc 4.9.3
 if [[ "$args" == *" mpi "* ]]; then
-  build_openmpi
+  build_openmpi 1.10.1
 fi
 
 # Pack output compiler tarball
