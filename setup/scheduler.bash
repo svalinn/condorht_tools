6b3f86a05c9e240986ad49b620fbb8be0efd317c
Lucas Jacobson <ljacobson64@gmail.com>
2016-01-12 12:52:22 -0600

    changed scheduler file naming

--- setup/scheduler.bash
+++ setup/scheduler.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 check_inc=1   # Minutes to wait between checks to see if the tests havs finished
-tests_inc=30  # Minutes to wait between re-running the tests
+tests_inc=60  # Minutes to wait between re-running the tests
 
 check_inc=$(( $check_inc * 60 ))
 tests_inc=$(( $tests_inc * 60 ))
@@ -13,8 +13,9 @@ temp_dir=$results_dir/temp
 mkdir -p $results_dir
 mkdir -p $temp_dir
 
+test_id=0
 while true; do
-  start_epoch=$(date +%s)
+  test_id=$(( $test_id + 1 ))
 
   # Submit the tests on Condor
   ssh $CONDOR 'cd condorht_tools/setup && condor_submit dag_mcnp_tests.sub'
@@ -31,30 +32,39 @@ while true; do
   scp $CONDOR:condorht_tools/setup/results_*.tar.gz .
   ssh $CONDOR 'mv condorht_tools/setup/*.tar.gz results'
   tarball=`echo results_*.tar.gz`
-  folder="${tarball:8:${#tarball}-15}"
+  datetime=${tarball:8:${#tarball}-15}
+  output=diff_$datetime
   tests=`tar --wildcards -tf $tarball */diff_summary | sed 's/\/.*//'`
   mv $tarball $results_dir
 
   # Extract the diff information
   cd $results_dir
-  mkdir $folder
+  rm -f $output
+  echo DAG-MCNP test suite diffs > $output
+  echo Test ID: $test_id >> $output
+  echo $datetime >> $output
   for test in $tests; do
     tar -xzvf $tarball $test/diff_summary
-    mv $test/diff_summary $folder/diff_summary_$test
+    echo >> $output
+    echo $test >> $output
+    cat $test/diff_summary >> $output
     rm -rf $test
   done
+  cp -f $output diff_most_recent
 
   # Copy the diff information to the CNERG web server
   web_dir=/home/vhosts/cnergdata.engr.wisc.edu/html/DAGMC-tests/diff_summaries
   if [[ $(hostname -f) == tux-*.cae.wisc.edu ]]; then
-    cp -r $folder $web_dir
+    cp -f $output diff_most_recent $web_dir
+    chmod 640 $web_dir/$output $web_dir/diff_most_recent
   else
-    scp -r $folder lucasj@best-tux.cae.wisc.edu:$web_dir
+    scp $output diff_most_recent lucasj@best-tux.cae.wisc.edu:$web_dir
+    ssh lucasj@best-tux.cae.wisc.edu 'chmod 640 $web_dir/$output $web_dir/diff_most_recent'
   fi
 
   # Wait until it's time to run the tests again
   end_epoch=$(date +%s)
-  sleep_seconds=$(( $start_epoch + $tests_inc - $end_epoch ))
+  sleep_seconds=$(( $test_id * $tests_inc + $start_epoch - $end_epoch ))
   if [[ $sleep_seconds < 0 ]]; then
     sleep_seconds=0
   fi
