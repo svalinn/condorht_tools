7363b1a2ad147eb4cdb760ea3e4d450e516f6536
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-08-22 15:55:47 -0500

    add cyclus_test

---
+++ setup/cyclus_test.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+export args="$@"
+export args=" "$args" "
+
+source ./common.bash
+source ./build_funcs.bash
+set_dirs
+set_versions
+set_env
+export make_install_tarballs=false
+export jobs=12
+
+# Cleanup directories
+rm -rf $build_dir $install_dir
+mkdir -p $dist_dir $build_dir $install_dir $copy_dir $DATAPATH
+
+# Make sure all the dependencies are built
+packages=(python gcc mpc mpfr gmp hdf5 boost Cbc sigcpp sqlite xml2 pcre glib glibmm xmlpp)
+for name in "${packages[@]}"; do
+  eval version=\$"$name"_version
+  echo Ensuring build of $name-$version ...
+  ensure_build $name $version
+done
+
+# recover cyclus and cycamore
+packages=(cyclus cycamore)
+for name in "${packages[@]:1}"; do
+  eval version=\$"$name"_version
+  echo Ensuring build of $name-$version ...
+  ensure_build $name $version
+done
+
+cyclus_unit_test
+cycamore_unit_test
